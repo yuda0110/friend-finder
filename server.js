eeaed2f711dd95a2aa30f76e102405b46470fa69
Yuko Uda <yuko.uda0110@gmail.com>
2020-05-05 11:18:43 -0700

    Implement htmlRoutes and server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const htmlRoutes = require("./app/routing/htmlRoutes");
+const apiRoutes = require("./app/routing/apiRoutes.js");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("app/public"))
+
+
+htmlRoutes(app);
+// apiRoutes(app)
+
+
+app.listen(PORT, function(){
+  console.log("app is listening on http://localhost:" + PORT);
+})
