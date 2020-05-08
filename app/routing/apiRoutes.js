4a6d7f9ff032d60a0620c38295e23d0262220bac
Yuko Uda <yuko.uda0110@gmail.com>
2020-05-07 17:01:29 -0700

    Implement POST form function on survey.html, and work on writing file function for friends data (NOT solved yet)

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,37 @@
 const fs = require("fs");
+const path = require("path");
 const friendsData = require("../data/friends.js");
 
 
 function apiRoutes(app) {
-  app.get("/api/friends", (req, res) => {
-    return res.json(friendsData)
+  console.log('apiRoutes!!');
+
+  app.get('/api/friends', (req, res) => {
+    console.log(' app.get(\'/api/friends\'');
+    console.log(friendsData());
+    return res.json(friendsData());
   });
 
-  app.post("/api/friends" , (req, res) => {
+  app.post('/api/friends' , (req, res) => {
+    console.log('post api friends!');
+
     const newFriend = req.body;
     friendsData.push(newFriend);
+    console.log(friendsData());
+
+    fs.writeFile(path.join(__dirname, '../data/friends.json'), JSON.stringify(friendsData, null, 2), (err) => {
+
+      // If an error was experienced we will log it.
+      if (err) {
+        console.log(err);
+      }
+
+      // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+      else {
+        console.log("A new friend Added!");
+      }
+
+    });
   });
 }
 
