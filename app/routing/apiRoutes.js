1529c9250d23096e28656aeb1d56d8b6c6a48df5
Yuko Uda <yuko.uda0110@gmail.com>
2020-05-08 16:08:25 -0700

    Create a separate function for findBestMatch

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,46 +13,45 @@ function apiRoutes(app) {
     console.log('post api friends!');
 
     const newFriend = req.body;
-    // friendsData.push(newFriend);
+    const bestMatch = findBestMatch(newFriend, friendsData);
+    console.log(bestMatch);
 
-    const ansArr = newFriend.scores;
-    console.log(ansArr);
-
-    const diffArr = [];
-    for (const data of friendsData) {
-      const otherAnsArr = data.scores;
-      console.log(data.name);
-
-      let diff = 0;
-
-      otherAnsArr.forEach((ans, index) => {
-        diff += Math.abs(ansArr[index] - ans);
-      })
-
-      diffArr.push(diff);
-    }
-
-    console.log(`diffArr: ${diffArr}`);
-    const minDiff = Math.min.apply(null, diffArr);
-    const minDiffIndexArr = [];
-    diffArr.forEach((num, index) => {
-      if (num === minDiff) {
-        minDiffIndexArr.push(index);
-      }
-    })
-
-    console.log('minDiffIndexArr: ' + minDiffIndexArr);
-
-    console.log('index: ' + minDiffIndexArr[Math.floor(Math.random() * minDiffIndexArr.length)]);
-
-    const matchFriendObj = friendsData[minDiffIndexArr[Math.floor(Math.random() * minDiffIndexArr.length)]];
-
-    // res.json({ id: result.insertId });
+    res.json(bestMatch);
   });
 }
 
-function findBestMatch(data) {
+function findBestMatch(newFriend, savedFriends) {
+  const ansArr = newFriend.scores;
+  console.log(ansArr);
 
+  const diffArr = [];
+  for (const data of savedFriends) {
+    const otherAnsArr = data.scores;
+    console.log(data.name);
+
+    let diff = 0;
+
+    otherAnsArr.forEach((ans, index) => {
+      diff += Math.abs(ansArr[index] - ans);
+    })
+
+    diffArr.push(diff);
+  }
+
+  console.log(`diffArr: ${diffArr}`);
+  const minDiff = Math.min.apply(null, diffArr);
+  const minDiffIndexArr = [];
+  diffArr.forEach((num, index) => {
+    if (num === minDiff) {
+      minDiffIndexArr.push(index);
+    }
+  })
+
+  console.log('minDiffIndexArr: ' + minDiffIndexArr);
+
+  console.log('index: ' + minDiffIndexArr[Math.floor(Math.random() * minDiffIndexArr.length)]);
+
+  return savedFriends[minDiffIndexArr[Math.floor(Math.random() * minDiffIndexArr.length)]];
 }
 
 module.exports = apiRoutes;
