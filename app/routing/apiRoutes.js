c275a177846bd4c801dc045a52592933d0777257
Yuko Uda <yuko.uda0110@gmail.com>
2020-05-08 15:49:47 -0700

    Write logic for finding best match from friendsData

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,9 +13,46 @@ function apiRoutes(app) {
     console.log('post api friends!');
 
     const newFriend = req.body;
-    friendsData.push(newFriend);
-    console.log(friendsData);
+    // friendsData.push(newFriend);
+
+    const ansArr = newFriend.scores;
+    console.log(ansArr);
+
+    const diffArr = [];
+    for (const data of friendsData) {
+      const otherAnsArr = data.scores;
+      console.log(data.name);
+
+      let diff = 0;
+
+      otherAnsArr.forEach((ans, index) => {
+        diff += Math.abs(ansArr[index] - ans);
+      })
+
+      diffArr.push(diff);
+    }
+
+    console.log(`diffArr: ${diffArr}`);
+    const minDiff = Math.min.apply(null, diffArr);
+    const minDiffIndexArr = [];
+    diffArr.forEach((num, index) => {
+      if (num === minDiff) {
+        minDiffIndexArr.push(index);
+      }
+    })
+
+    console.log('minDiffIndexArr: ' + minDiffIndexArr);
+
+    console.log('index: ' + minDiffIndexArr[Math.floor(Math.random() * minDiffIndexArr.length)]);
+
+    const matchFriendObj = friendsData[minDiffIndexArr[Math.floor(Math.random() * minDiffIndexArr.length)]];
+
+    // res.json({ id: result.insertId });
   });
+}
+
+function findBestMatch(data) {
+
 }
 
 module.exports = apiRoutes;
