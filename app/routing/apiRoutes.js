a237e1e5a450d953f5d0479fc9409414d772022c
Yuko Uda <yuko.uda0110@gmail.com>
2020-05-08 17:24:53 -0700

    Delete unnecessary codes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,20 +1,14 @@
-const path = require("path");
 const friendsData = require("../data/friends.js");
 
 
 function apiRoutes(app) {
-  console.log('apiRoutes!!');
-
   app.get('/api/friends', (req, res) => {
     return res.json(friendsData);
   });
 
   app.post('/api/friends' , (req, res) => {
-    console.log('post api friends!');
-
     const newFriend = req.body;
     const bestMatch = findBestMatch(newFriend, friendsData);
-    console.log(bestMatch);
 
     res.json(bestMatch);
   });
@@ -22,13 +16,9 @@ function apiRoutes(app) {
 
 function findBestMatch(newFriend, savedFriends) {
   const ansArr = newFriend.scores;
-  console.log(ansArr);
-
   const diffArr = [];
   for (const data of savedFriends) {
     const otherAnsArr = data.scores;
-    console.log(data.name);
-
     let diff = 0;
 
     otherAnsArr.forEach((ans, index) => {
@@ -46,11 +36,6 @@ function findBestMatch(newFriend, savedFriends) {
       minDiffIndexArr.push(index);
     }
   })
-
-  console.log('minDiffIndexArr: ' + minDiffIndexArr);
-
-  console.log('index: ' + minDiffIndexArr[Math.floor(Math.random() * minDiffIndexArr.length)]);
-
   return savedFriends[minDiffIndexArr[Math.floor(Math.random() * minDiffIndexArr.length)]];
 }
 
